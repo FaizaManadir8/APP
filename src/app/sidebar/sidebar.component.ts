547acadd3e518111c4bdfd7a7d880eb4ddd7b9e6
kaoutar-karchaou <kaoutar.karchaou@opencellsoft.com>
2022-05-17 14:42:33 +0100

    fix

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatSidenav} from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { delay, filter } from 'rxjs/operators';
 import { Compte } from 'src/app/login/login.component';
@@ -9,71 +9,74 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-  compte !: Compte;
-  isAgent:boolean=false;
-  isChefService:boolean=false;
-  isResponsableRH:boolean=false;
-  
-  url="assets/avatar6.png"
-  onselectFile(e:any){
-    if(e.target.files){
+  compte!: Compte;
+  isAgent: boolean = false;
+  isChefService: boolean = false;
+  isResponsableRH: boolean = false;
+
+  url = 'assets/avatar6.png';
+  onselectFile(e: any) {
+    if (e.target.files) {
       var reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
-      reader.onload=(event:any)=>{
-        this.url=event.target.result;
-      }
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
     }
-
-    
   }
-  
 
   @ViewChild(MatSidenav)
-  sidenav!: MatSidenav; 
-  
-    constructor(private observer: BreakpointObserver,private loginService : LoginService,private router: Router) {
-      this.getUserLoggedIn();
-    }
-    getUserLoggedIn() {
-      if (localStorage.getItem('user')) {
-      this.compte = JSON.parse(localStorage.getItem('user')!);
-       
-      if(this.compte){
-        console.log("compte", this.compte);  
-        if(this.compte.privilege === "Agent"){
-          this.isAgent = true;
-        }else if(this.compte.privilege === "ChefDeService"){
-          this.isChefService = true;
-        }else(this.compte.privilege === "ResponsableRH")
-          this.isResponsableRH = true;
-        }
-      }
-      
-    }
-    
-    ngAfterViewInit() {
-      this.observer
-        .observe(['(max-width: 800px)'])
-        .subscribe((res) => {
-          if (res.matches) {
-            this.sidenav.mode = 'over';
-            this.sidenav.close();
-          } else {
-            this.sidenav.mode = 'side';
-            this.sidenav.open();
-          }
-        });
-       
-       
+  sidenav!: MatSidenav;
 
-      }
-  ngOnInit(): void {
+  constructor(
+    private observer: BreakpointObserver,
+    private loginService: LoginService,
+    private router: Router
+  ) {
+    this.getUserLoggedIn();
   }
+  getUserLoggedIn() {
+    if (localStorage.getItem('user')) {
+      this.compte = JSON.parse(localStorage.getItem('user')!);
+
+      if (this.compte) {
+        console.log('compte', this.compte);
+        if (this.compte.privilege === 'Agent') {
+          this.isAgent = true;
+          this.isChefService = false;
+          this.isResponsableRH = false;
+        } else if (this.compte.privilege === 'ChefDeService') {
+          this.isAgent = false;
+          this.isChefService = true;
+          this.isResponsableRH = false;
+        } else if (this.compte.privilege === 'ResponsableRH')
+        {this.isAgent = false;
+        this.isChefService = false;
+        this.isResponsableRH = true;
+        }}
+    }
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+  ngOnInit(): void {}
   logout() {
-    this.loginService.clearLocalStorage(); 
+    this.loginService.clearLocalStorage();
     this.router.navigate(['/login']);
-   }
+    this.isAgent = false;
+    this.isChefService = false;
+    this.isResponsableRH = false;
+  }
 }
