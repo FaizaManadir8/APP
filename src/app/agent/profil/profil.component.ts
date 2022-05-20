144823f49630507dd075857a6e9c2de34bd0e207
kaoutar-karchaou <kaoutar.karchaou@opencellsoft.com>
2022-05-20 17:54:56 +0100

    view leave request details || view user account

--- src/app/agent/profil/profil.component.ts
+++ src/app/agent/profil/profil.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Compte } from 'src/app/login/login.component';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  compte! : Compte;
+  constructor( private httpClient: HttpClient,) { }
 
   ngOnInit(): void {
+      if (localStorage.getItem('user')) {
+        this.compte = JSON.parse(localStorage.getItem('user')!);
+        this.getUser();
+  }
+}
+
+  getUser(){
+    this.httpClient.get(`http://localhost:8080/comptes/get/${this.compte.id}`).subscribe((response)=>{
+    this.compte=response;
+    console.log("response",response)
+    });
   }
 
-}
+  }
+
