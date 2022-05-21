e17556639a8e0a1f145355e07cf97a3f7e53fc51
ikramonari1 <ikramomari990@gmail.com>
2022-05-21 11:25:22 +0100

    kk

--- src/app/responsable/lesdecisions/lesdecisions.component.ts
+++ src/app/responsable/lesdecisions/lesdecisions.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatDialog} from "@angular/material/dialog";
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { DeleteCongeComponent } from 'src/app/delete-conge/delete-conge.component';
+import { UpdateCongeComponent } from 'src/app/update-conge/update-conge.component';
+import { HttpClient } from '@angular/common/http';
+
+export interface ListeDecision {
+  id: number;
+  nombreDeJours: number;
+  dateDemande: Date;
+  dateDepart: Date;
+  dateRetour: Date;
+  etat:string;
+}
 
 @Component({
   selector: 'app-lesdecisions',
@@ -7,9 +23,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LesdecisionsComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id','nombreDeJours', 'dateDemande', 'dateDepart','dateRetour', 'etat', 'action'];
+  dataSource! : MatTableDataSource<ListeDecision>;
+  decision:any;
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+
+  constructor(private httpClient: HttpClient,public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.httpClient
+    .get('http://localhost:8080/decision/get')
+    .subscribe((response: any) => {
+      console.log('response', response);
+      // console.log(response[0].id);
+      this.dataSource = new MatTableDataSource(response);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
+  applyFilter(decision:any) {
+    const filterValue = (decision.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  editDecision(decision:any) {
+
+    const dialogRef = this.dialog.open(UpdateCongeComponent, {
+      width: '450px',
+      data: decision
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.httpClient.put('http://localhost:8080/decision/update',result).subscribe(response => {});
+      this.decision = decision;
+    });
+   
+   }
+   deleteDecision(decision:any){
+    const dialogRef = this.dialog.open(DeleteCongeComponent, {
+      width: '450px',
+      data: decision
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      const url = `http://localhost:8080/decision/delete/${result.id}`;
+      this.httpClient.delete(url).subscribe(response => {
+        console.log("response",response);
+        this.ngOnInit();
+      });
+      this.decision = decision;
+    });
+   }
 }
